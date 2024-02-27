e6a1556edfa1c8d62a74ed882657fb7ea2fbbc35
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2024-02-27 22:13:57 +0100

    Update image_similitary.py

--- image_similitary.py
+++ image_similitary.py
@@ -1,53 +1,47 @@
 import cv2 as cv
-import numpy as np
-import cvzone
-from cvzone.SelfiSegmentationModule import SelfiSegmentation
+import os
 
-base = cv.imread("HandTrackingIA/Hands/a.png")
-test = cv.imread("HandTrackingIA/A.png")
-test2 = cv.imread("HandTrackingIA/Hands/c.png")
+def compare_images(base_path, test_path):
+    base = cv.imread(base_path)
+    hsv_base = cv.cvtColor(base, cv.COLOR_BGR2HSV)
+    hist_base = cv.calcHist([hsv_base], [0, 1], None, [50, 60], [0, 180, 0, 256])
+    cv.normalize(hist_base, hist_base, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+    
+    test = cv.imread(test_path)
+    hsv_test = cv.cvtColor(test, cv.COLOR_BGR2HSV)
+    hist_test = cv.calcHist([hsv_test], [0, 1], None, [50, 60], [0, 180, 0, 256])
+    cv.normalize(hist_test, hist_test, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+    
+    similarity = cv.compareHist(hist_base, hist_test, cv.HISTCMP_CORREL)
+    
+    return similarity
 
+def main():
+    base_folder = "./Hand"
+    base_files = os.listdir(base_folder)
+    training_folder = "data/training"
+    training_subfolders = [os.path.join(training_folder, subfolder) for subfolder in ['A', 'B', 'C', 'D']]
+    max_images = 1
+    
+    with open("word.txt", "w") as file:
+        for subfolder in training_subfolders:
+            print(f"\nComparaison pour le sous-dossier : {os.path.basename(subfolder)}")
+            
+            for i in range(1, max_images + 1):
+                test_image_path = os.path.join(subfolder, f"{i}.jpg")
+                
+                if os.path.isfile(test_image_path):
+                    for base_file in base_files:
+                        base_image_path = os.path.join(base_folder, base_file)
+                        similarity = compare_images(base_image_path, test_image_path)
+                        print(f"Similarité entre {os.path.basename(base_image_path)} et {os.path.basename(test_image_path)} : {similarity}")
+                        
+                        if similarity > 0.95:
+                            file.write(f"{os.path.basename(base_image_path)} : {os.path.basename(subfolder)}\n")
+                    
+                    print()
+                else:
+                    print(f"Fichier {test_image_path} non trouvé, saut de comparaison.")
 
-hsv_base = cv.cvtColor(base, cv.COLOR_BGR2HSV)
-hsv_test = cv.cvtColor(test, cv.COLOR_BGR2HSV)
-hsv_test2 = cv.cvtColor(test2, cv.COLOR_BGR2HSV)
-
-h_bins = 50
-s_bins = 60
-histSize = [h_bins, s_bins]
-h_ranges = [0, 180]
-s_ranges = [0, 256]
-ranges = h_ranges + s_ranges
-channels = [0, 1]
-
-hist_base = cv.calcHist([hsv_base], channels, None, histSize, ranges, accumulate=False)
-cv.normalize(hist_base, hist_base, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
-hist_test = cv.calcHist([hsv_test], channels, None, histSize, ranges, accumulate=False)
-cv.normalize(hist_test, hist_test, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
-hist_test2 = cv.calcHist(
-    [hsv_test2], channels, None, histSize, ranges, accumulate=False
-)
-cv.normalize(hist_test2, hist_test2, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
-
-compare_method = cv.HISTCMP_CORREL
-
-base_base = cv.compareHist(hist_base, hist_base, compare_method)
-base_test = cv.compareHist(hist_base, hist_test, compare_method)
-base_test2 = cv.compareHist(hist_base, hist_test2, compare_method)
-
-print("base_base Similarity = ", base_base)
-print("base_test Similarity = ", base_test)
-print("base_test2 Similarity = ", base_test2)
-
-if base_test > 0.985:
-    print("BASE TEST 1: \nThe similarity between the base image are very near !")
-elif base_test2 > 0.985:
-    print("BASE TEST 2: \nThe similarity between the base image are very near !")
-else:
-    print("NONE:\nThe base image are not silimitarity with the other images !")
-
-
-cv.imshow("base", base)
-cv.imshow("test1", test)
-cv.imshow("test2", test2)
-cv.waitKey(0)
+if __name__ == "__main__":
+    main()
